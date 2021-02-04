467c32d1d0280603d6acf07c60ac2930e1b7aa54
milandak19 <fiqrimilano@gmail.com>
2021-02-04 19:30:46 +0700

    skeleton controller sama router udah dibikin

---
+++ controllers/controller.js
@@ -0,0 +1,16 @@
+class Controller {
+  static login(req, res, next) {
+
+  }
+  static getComic(req, res, next) {
+
+  }
+  static getManga(req, res, next) {
+
+  }
+  static getGame(req, res, next) {
+
+  }
+}
+
+module.exports = Controller
